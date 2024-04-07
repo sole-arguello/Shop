fe52778db0de1774738e47b887daa980cf81ed67
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-04-07 12:23:32 -0300

    add api products

--- Client/src/context/GlobalState.jsx
+++ Client/src/context/GlobalState.jsx
@@ -1,9 +1,12 @@
 import { createContext } from 'react';
+import ProductsApi from '../api/ProductsApi';
 
 
 export const GlobalContext = createContext();
 
 export const DataProvider = ({ children }) => {
+
+    ProductsApi()
     return (
         <GlobalContext.Provider value={"value"}>
             {children}
