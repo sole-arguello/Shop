fa9e0883d20165166a8d511ba1070c5db705c986
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-04-08 00:20:09 -0300

    add funcinality

--- Client/src/components/mainPages/auth/Login.jsx
+++ Client/src/components/mainPages/auth/Login.jsx
@@ -1,9 +1,48 @@
-
+import  { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function Login() {
+ 
+  const [userInfo, setUserInfo] = useState({
+    email: '',
+    password: ''
+  });
+
+  const handleChangeInput = ({target}) => {
+    const {name, value} = target;
+    setUserInfo({...userInfo, [name]: value});
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('info', userInfo);
+    try {
+      const response = await axios.post('/api/login', {...userInfo});
+      console.log('response', response, "user", userInfo);
+    } catch (error) {
+      console.log('error', error);
+      alert(error.response.data.message);
+    }
+
+  }
   return (
-    <div>Login Component</div>
-  )
+    <div className='login-page' onSubmit={handleSubmit}>
+      <form >
+        <input type="email" name='email' required placeholder='Email'
+        value={userInfo.email} onChange={handleChangeInput}/>
+
+        <input type="password" name='password' required placeholder='Password'
+        value={userInfo.password} onChange={handleChangeInput}/>
+
+        <div className='row'>
+          <button type='submit'>Login</button>
+          <Link to='/register'>Register</Link>
+        </div>
+      </form>
+
+    </div>
+  );
 }
 
-export default Login
+export default Login;
