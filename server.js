47045cc65b5b244f5522d59ac1d55d4d60ba38e3
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-04-08 00:13:09 -0300

    fix port

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/', (req, res) => {
     res.json({msg: 'Hello World'})
 })
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
