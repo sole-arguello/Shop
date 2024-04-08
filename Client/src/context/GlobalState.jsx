99bfedbe36de1afd271f67d663624df6fcf786b4
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-04-08 00:17:18 -0300

    add funcionality the context

--- Client/src/context/GlobalState.jsx
+++ Client/src/context/GlobalState.jsx
@@ -1,14 +1,19 @@
-import { createContext } from 'react';
-import ProductsApi from '../api/ProductsApi';
-
-
+import { createContext, useState } from 'react';
+import ProductAPI from '../api/ProductAPI';
 export const GlobalContext = createContext();
 
 export const DataProvider = ({ children }) => {
+    const [token, setToken] = useState(false)
 
-    ProductsApi()
+    ProductAPI()
+
+    const state = {
+        token: [token, setToken],
+        productsApi: ProductAPI()
+    } 
+    //console.log('estado', state)
     return (
-        <GlobalContext.Provider value={"value"}>
+        <GlobalContext.Provider value={state}>
             {children}
         </GlobalContext.Provider>
     )
