9b28e61f7639694e277c99c92bcf610ee2297a75
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-04-09 19:24:46 -0300

    fix variable

--- middlewares/authAdmin.js
+++ middlewares/authAdmin.js
@@ -2,10 +2,13 @@ import { User } from "../models/userModels.js";
 
 export const authAdmin = async (req, res, next) => {
     try {
-        const user = await User.findOne({ _id: req.user.id });
+
+        const user = await User.findOne({ id: req.user_id });
+        console.log("user admin", user)
         if (user.role === 0) return res.status(400).json({ msg: "Admin resources access denied." });
         next();
     } catch (error) {
+        console.log(error)
         return res.status(500).json({ msg: error.message });
         
     }
