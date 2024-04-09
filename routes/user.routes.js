f39f02b035b1dc4d6a69b4a008f32108cec0b04f
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-04-09 13:25:54 -0300

    fix route refresh call post for get

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -14,7 +14,7 @@ router.post('/login', UserController.login );
 router.get('/logout', UserController.logout );
 
 //localhost:5000/refresh_token
-router.post('/refresh_token', UserController.refreshToken );
+router.get('/refresh_token', UserController.refreshToken );
 
 //localhost:5000/user/infor
 router.get('/infor', auth, UserController.getUser );//perfil
