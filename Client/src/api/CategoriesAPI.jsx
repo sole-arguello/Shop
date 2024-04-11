6b323fcfb28b2fe5d61944469aed056924a08b73
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-04-11 19:43:40 -0300

    conction db categories API

---
+++ Client/src/api/CategoriesAPI.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+
+function CategoriesAPI() {
+
+  const [categories, setCategories] = useState([])
+  const [callback, setCallback] = useState(false)
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const res = await axios.get('/api/category')
+      console.log('res categorias', res.data.categories)
+      //setCategories(res)
+    }
+    getCategories()
+  }, [callback])
+  return (
+    {categories, setCategories, callback, setCallback}
+  )
+}
+
+export default CategoriesAPI
