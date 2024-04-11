94e9f68a63ba596ae9d99c3dd72abbc7851fd1d5
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-04-11 00:36:28 -0300

    add funcionlity cart and crud

--- Client/src/components/mainPages/utils/Product_Item/ProductItem.jsx
+++ Client/src/components/mainPages/utils/Product_Item/ProductItem.jsx
@@ -1,10 +1,20 @@
+import { GlobalContext } from '../../../../context/GlobalState'
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import './ProductItem.css'
 
 
-function ProductItem({product}) {
+function ProductItem({product, isAdmin}) {
+
+  const { state } = useContext(GlobalContext)
+  console.log('Estado en product item', state.userApi)
+  const { addCart } = state.userApi
+  console.log('addCart', addCart)
+
+
   return (
     <div className="product_card">
+        {isAdmin && <input type="checkbox" checked={product.checked} />}
         <img src={product.images.url} alt={product.title} />
         <div className="product_box"> 
             <h2>{product.title}</h2>
@@ -12,8 +22,17 @@ function ProductItem({product}) {
             <p>{product.description}</p>
         </div>
         <div className="row_btn">
-            <Link id="btn_buy" to="#!">Buy </Link>
-            <Link id="btn_view" to={`/detail/${product._id}`}>View </Link>
+            { isAdmin 
+            ? <>
+                <Link id="btn_buy" to="#!">DELETE </Link>
+                <Link id="btn_view" to={`/edit_product/${product._id}`}>EDIT </Link>
+              </> 
+            : <>
+                <Link id="btn_buy" to="#!" onClick={() => addCart(product)}>Buy </Link>
+                <Link id="btn_view" to={`/detail/${product._id}`}>View </Link>            
+              </>
+            }
+       
         </div>
     </div>
   )
