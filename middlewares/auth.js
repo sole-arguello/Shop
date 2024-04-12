b81c35f1cfdfbdc7e5370805c2c29d8ad3f1625a
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-04-12 16:07:33 -0300

    debug

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,10 +4,10 @@ export const auth = (req, res, next) => {
     try {
         const token = req.header('Authorization')
         console.log('Token Auth', token)
-        if(!token) return res.status(400).json({ msg: 'Invalid Authentication.' })
-
+        if(!token) return res.status(400).json({ msg: 'Invalid Authentication 1.' })
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-            if(err) return res.status(400).json({ msg: 'Invalid Authentication.' })
+            console.log('User verif',user, err, 'token pos', token)
+            if(err) return res.status(400).json({ msg: 'Invalid Authentication 2.' })
             req.user = user
             next()
         })
