91cc4bf4067fff6f77afe6c5940656e41ba94a8e
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-04-13 11:42:50 -0300

    modify limit of products

--- controlles/product.controller.js
+++ controlles/product.controller.js
@@ -32,7 +32,7 @@ class APIfeature{
     }
     paginating(){
         const page = this.queryString.page * 1 || 1;
-        const limit = this.queryString.limit * 1 || 3;
+        const limit = this.queryString.limit * 1 || 0;
         const skip = (page - 1) * limit;
         this.query = this.query.skip(skip).limit(limit);
         return this
