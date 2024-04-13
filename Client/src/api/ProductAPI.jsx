18d668f7eb4607732e2e8b03572695fc3e15beb1
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-04-13 19:05:29 -0300

    add get api products

--- Client/src/api/ProductAPI.jsx
+++ Client/src/api/ProductAPI.jsx
@@ -1,25 +1,14 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState } from 'react'
+
 
 function ProductAPI() {
 
     const [products, setProducts] = useState([])
 
-    const getProducts = async () => {
-      try {
-        const res = await axios.get('/api/products');
-        //console.log('res', res.data.products)
-        setProducts(res.data.products);
-      } catch (error) {
-          console.error('Error fetching products:', error);
-      }
-    }
+    /// aqui estaba el bloque que pase en el componente products//////
 
-    useEffect(() => {
-        getProducts()
-    }, [])
   return (
-    {products}
+    {products, setProducts}
   )
 }
 
