a726d8c800ac2a4ff94a2bb7061dffd0cf1152f8
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-04-12 22:31:46 -0300

    add path component Create Product

--- Client/src/components/mainPages/Pages.jsx
+++ Client/src/components/mainPages/Pages.jsx
@@ -1,15 +1,15 @@
 
 import { Routes, Route } from 'react-router-dom';
-
+import { GlobalContext } from '../../context/GlobalState';
+import { useContext } from 'react';
 import Products from "./products/Products"
 import Login from "./auth/Login"
 import Register from "./auth/Register"
 import Cart from "./cart/Cart"
 import NotFound from "./utils/NotFound"
 import DetailProduct from './products/detailProduct/DetailProduct';
-import { GlobalContext } from '../../context/GlobalState';
-import { useContext } from 'react';
 import Categories from './categories/Categories';
+import CreateProduct from './createproduct/CreateProduct';
 
 function Pages() {
   const state = useContext(GlobalContext)
@@ -22,6 +22,7 @@ function Pages() {
     <Route path="/login" element={ isLogged ? <NotFound /> : <Login />} />
     <Route path="/register" element={ isLogged ? <NotFound /> : <Register />} />
     <Route path="/category" element={ isLogged ? <Categories /> : <NotFound />} />
+    <Route path='/create_product' element={isLogged ? <CreateProduct /> : <NotFound />} />
     <Route path="/cart" element={<Cart />} />
     <Route path="/detail/:id" element={<DetailProduct />} />
 
