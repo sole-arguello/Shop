6f80d98c5b8bf0393e5dacc7f44a3b56825218fc
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-04-13 19:06:48 -0300

    add call api products

--- Client/src/components/mainPages/products/Products.jsx
+++ Client/src/components/mainPages/products/Products.jsx
@@ -1,18 +1,39 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { GlobalContext } from '../../../context/GlobalState';
 import ProductItem from '../utils/Product_Item/ProductItem';
-import './Products.css';
 import Loading from '../utils/Loading/Loading';
+import axios from 'axios';
+import './Products.css';
+
 
 function Products() {
     const state = useContext(GlobalContext);
-    //console.log('Estado products', state)
-    const { products } = state.productsApi;
-    //console.log('products', products)
+    console.log('Estado componente products', state)
+    const { products, setProducts } = state.productsApi;
+    console.log('productos componente', products)
     const { isAdmin } = state.userApi
     console.log('Products isAdmin', isAdmin)
     // const { addCart } = state.userApi
     // console.log('addCart in products', addCart)
+
+    //////Este bloque estaba en products.API////
+    const getProducts = async () => {
+        try {
+          const res = await axios.get('/api/products');
+          //console.log('res', res.data.products)
+          setProducts(res.data.products);
+        } catch (error) {
+            console.error('Error fetching products:', error);
+            alert(error.response.data.message);
+        }
+      }
+  
+      useEffect(() => {
+          getProducts()
+      }, [])
+
+      //////////////////////////////////////////////
+    
 
     return (
         <>
